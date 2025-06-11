950f1d6980a6bd24c65a4a81f1cc4b6582367d6f
ivanbch <167141081+ivanbch@users.noreply.github.com>
2025-06-11 16:04:10 -0300

    update

--- src/navegacion_auto_v2.ino
+++ src/navegacion_auto_v2.ino
@@ -1,20 +1,28 @@
 #include <ESP8266WiFi.h>
-#define motor_izq_a   D8
-#define motor_izq_b   D7
-#define motor_der_a   D6
-#define motor_der_b   D5
-#define pinLed        D4
-#define Trigger       D2
-#define Echo          D1
-#define tiempoGiro    1500
-#define tiempoAvance  5000
-#define umbral        15
+#define motor_der_a    D8
+#define motor_der_b    D7
+#define motor_izq_a    D6
+#define motor_izq_b    D5
+#define pinLed         D4
+#define Trigger        D2
+#define Echo           D1
+#define pinVoltage     A0
+#define tiempoAvance   5000
+#define umbral         15
+#define tiempoGiroAnti 1000
+#define tiempoGiroHor 1100
+const int v_max = 1;  // Velocidad tope (la más rápida)
+const int v_min = 0.25;  // Velocidad más lenta
+const float R1 = 10000.0; //  ohms
+const float R2 = 5000.0;  //  ohms
 bool reversa = false;
 const char* nombre_wifi = "Robotin";
 const char* contra = "robotin123";
 long duracion;
 int distancia;
 bool manual = true; 
+int v; //es un porcentaje de la velocidad maxima
+int i;
 /*
 
 creo que lo mejor es usar el rover como Access Point
@@ -24,11 +32,6 @@ IPAddress ip_rover(192,168,4,1);
 IPAddress puerta_enlace(192,168,4,1);        // Gateway (igual que la IP en modo punto de acceso)
 IPAddress mascara(255,255,255,0);        // Máscara de subred
 
-//IPAddress ip_rover (192,168,253,10);        // (si quiero usar el hotspot como router)
-//IPAddress ip_rover (192,168,137,10);        // (si quiero usar mi pc como router)
-//IPAddress puerta_enlace (192,168,253,154); // (si quiero usar el hotspot como router)
-//IPAddress puerta_enlace (192,168,137,1); // (si quiero usar mi pc como router)
-//IPAddress mascara(255,255,255,0);
 
 struct motor {
   int pin1;
@@ -38,10 +41,79 @@ struct motor {
 motor motor_izq {motor_izq_a,motor_izq_b};
 motor motor_der {motor_der_a,motor_der_b};
 
+void VoltBateria () {
+  // si 1023 es 8.4 -> valor = analogWrite (A0) * 8.4/1023 
+  int val = analogRead (A0);
+  Serial.print("Valor digital batería: ");
+  Serial.println (val);
+  Serial.print("Volts batería: ");
+  Serial.println(val * 0.1144); // Lee el valor analógico del pin A0
+  
+  /*
+  float voltajePin = valorADC * (3.3 / 1023.0); // Convierte a voltaje ya que el rango es 0-1V, supuestamente la d1 mini tiene resist internas que hacen que con 3.3V llegue 1V
+  float voltajeBateria = voltajePin * ((R1 + R2) / R2);
+  Serial.print ("Batería: ");
+  Serial.println (voltajeBateria);
+  Serial.print ("Porcentaje de batería: ");
+  Serial.println (voltajeBateria);
+  return voltajeBateria;
+  */
+}
+/*
+void acelerar () {
+  if (v < 0.9) {
+    v = v + 0.20;
+  }
+  tiempoGiroHor = tiempoGiroHorOrig / v;
+  tiempoGiroAnti = tiempoGiroAntiOrig / v;
+  Serial.print("Velocidad: ");
+  Serial.println (int(1023*v));
+  Serial.print ("Tiempo giro horario 90 grados: ");
+  Serial.println (tiempoGiroHor);
+  Serial.print ("Tiempo giro antihorario 90 grados: ");
+  Serial.println (tiempoGiroAnti);
+}
+void desacelerar () {
+  if (v > 0.3) {
+    v = v - 0.20;
+  }
+  tiempoGiroHor = tiempoGiroHorOrig / v;
+  tiempoGiroAnti = tiempoGiroAntiOrig / v;
+  Serial.print("Velocidad: ");
+  Serial.println (int(1023*v));
+  Serial.print ("Tiempo giro horario 90 grados: ");
+  Serial.println (tiempoGiroHor);
+  Serial.print ("Tiempo giro antihorario 90 grados: ");
+  Serial.println (tiempoGiroAnti);
+}
+*/
+//IPAddress ip_rover (192,168,253,10);        // (si quiero usar el hotspot como router)
+//IPAddress ip_rover (192,168,137,10);        // (si quiero usar mi pc como router)
+//IPAddress puerta_enlace (192,168,253,154); // (si quiero usar el hotspot como router)
+//IPAddress puerta_enlace (192,168,137,1); // (si quiero usar mi pc como router)
+//IPAddress mascara(255,255,255,0);
+//R1 = 5 KOhm = 10 Ohm,
+//Vout = Vin*R2/(R1+R2) -> Vout = Vin *(5/(15)) = 8,4 V * 1/3 = 2,8
+//Vin lo voy a obtener multiplicando 8,4 * vout?
+//recta analog a dig: y de 0 a 1024, x de 0 a 1v
+// cada bat toma 4,2v y max 8,4v. 
+
+int velocidades[5] = {10,15,30,50,100};
 WiFiServer server(80);
 int estadoLed = LOW;
 const long intervalo = 200;
 unsigned long millisAnterior = 0;
+
+void setearVel (int pos) {
+  v = velocidades[pos];
+  Serial.print ("Velocidad: ");
+  Serial.println (1023*v/100);
+  
+}
+
+int vActual () {
+  return v;
+}
 
 // funciones de motor
 void configurarMotor (motor m) {
@@ -49,20 +121,22 @@ void configurarMotor (motor m) {
   pinMode(m.pin2,OUTPUT);
 }
 
-void girarAdelante (motor m) {  //esto es para controlar el giro de un motor en específico hacia adelante, habría que confirmar
-  digitalWrite(m.pin1,HIGH);           // que sea el correcto, sino reasigno los pines en la instancia del motor y listo
+void girarAdelante (motor m) {  //esto es para controlar el giro de un motor en específico hacia adelante, habría que confirmar           // que sea el correcto, sino reasigno los pines en la instancia del motor y listo
   digitalWrite(m.pin2,LOW);
+  analogWrite(m.pin1,1023*v/100);
 }
 
 void girarAtras (motor m) {
   digitalWrite(m.pin1,LOW);
-  digitalWrite(m.pin2,HIGH);
+  analogWrite(m.pin2,1023*v/100);
 }
 
 void frenar (motor m) { //frena UN motor solo
   digitalWrite(m.pin1,LOW);
   digitalWrite(m.pin2,LOW);
 }
+
+// funciones auxiliares
 
 void prenderLed () {
   digitalWrite(pinLed,LOW);
@@ -86,17 +160,6 @@ int calcularDistancia() {
  return distancia;
 }
 
-
-int promedioDistancia () {  //como el sensor es muy sensible, voy a hacer un promedio de 3 mediciones antes de compararlas
-  int sum = 0;                                                                                              //con el umbral
-  for(int p = 0; p < 3;p++) {
-    sum = sum += calcularDistancia();
-    delay(60);  //según recomendaciones tecnicas
-  }
-  int prom = sum / 3;
-  return prom;
-}
-
 /*
 Funciones de navegación básica
 */
@@ -115,10 +178,10 @@ void girarDer () { //lo mismo pero hacia la derecha
 }
 
 void avanzar () {
-  girarAdelante (motor_izq);
-  girarAdelante (motor_der);
-  prenderLed ();
-  Serial.println("avanza");
+    girarAdelante (motor_izq);
+    girarAdelante (motor_der);
+    prenderLed ();
+    Serial.println("avanza");  
 } 
 
 void detenerse () {
@@ -142,17 +205,27 @@ void girarIzqMarchaAtras () { //rover gira hacia la izquierda marcha atrás; rev
   Serial.println("gira marcha atrás hacia la izquierda");
 }
 
-void girarHorario () { //rover gira hacia la izquierda marcha atrás; revisar sentido
+void girarHorario () {
+  int a = v; 
+  Serial.println(v*1023/100);
+  setearVel (0);
   girarAtras (motor_der);
   girarAdelante (motor_izq);
   prenderLed ();
+  v = a;
+  Serial.println(v*1023/100);
   Serial.println("gira en sentido horario");
 }
 
-void girarAntiHorario () { //rover gira hacia la izquierda marcha atrás; revisar sentido
+void girarAntiHorario () {
+  int a = v; 
+  Serial.println(a*1023/100);
+  setearVel (0);
   girarAtras (motor_izq);
   girarAdelante (motor_der);
   prenderLed ();
+  v = a;
+  Serial.println(v*1023/100);
   Serial.println("gira en sentido antihorario");
 }
 
@@ -164,43 +237,44 @@ void girarDerMarchaAtras () { //rover gira hacia la derecha marcha atrás; revis
 }
 
 // funciones autónomas
-
-bool girar90Izq () {
+/*
+bool evadirIzq () {
+  Serial.println ("Intenta evadir por izquierda.");
   int i = 0;
   detenerse();
   delay(500);
   girarAntiHorario ();
-  delay(tiempoGiro);
+  delay(tiempoGiroHor);
   detenerse ();
   delay(500);
-  int distancia = promedioDistancia ();
+  int distancia = calcularDistancia();
   bool frenteLibre = false;
   if (distancia > umbral) {
-    while (distancia > umbral && i < tiempoAvance && frenteLibre == false) {  
-      if (i%500 == 0 && i > 0) {    //explicado en la función de abajo
+    while (distancia > umbral && i < tiempoAvance && !frenteLibre) {  
+      if (i%1000 == 0 && i > 0) {    //explicado en la función de abajo
         girarHorario ();
-        delay(tiempoGiro);
+        delay(tiempoGiroHor);
         detenerse ();
         delay(500);
-        distancia = promedioDistancia ();
+        distancia = calcularDistancia ();
         if (distancia > umbral) {
           frenteLibre = true;
         } else {
           girarAntiHorario ();
-          delay (tiempoGiro);
+          delay (tiempoGiroAnti);
           detenerse();
           delay(500);
         }
       }
-      if (frenteLibre == false) {
+      if (!frenteLibre) {
         avanzar ();
-        delay (100);
-        i += 100;
+        delay (50);
+        i += 50;
         distancia = calcularDistancia ();
       }
       
     }
-    if (frenteLibre == false) {   
+    if (!frenteLibre) {   
       int tAvanz= i;       //avanzó una vez que dobló pero era muy poco (depende del ancho del objeto)
       i = 0;
       detenerse ();
@@ -211,7 +285,7 @@ bool girar90Izq () {
       delay(500);
       i = 0;
       girarHorario ();   
-      delay(tiempoGiro);
+      delay(tiempoGiroHor);
       detenerse();
     } 
   } else {        
@@ -219,10 +293,10 @@ bool girar90Izq () {
       delay(500);
       i = 0;
       girarHorario ();                 //deshace el giro (no llegó a avanzar)
-      delay(tiempoGiro);
+      delay(tiempoGiroHor);
       detenerse();
     }
-  if (frenteLibre == true) {
+  if (frenteLibre) {
     Serial.println("giró bien izq 90");
   } else {
     Serial.println("NO giró izq 90");
@@ -230,29 +304,30 @@ bool girar90Izq () {
   return frenteLibre;
 }
 
-bool girar90Der () {
+bool evadirDer () {
+  Serial.println ("Intenta evadir por derecha.");
   int i = 0;
   detenerse();
   delay (500);
   girarHorario ();
-  delay(tiempoGiro);
+  delay(tiempoGiroHor);
   detenerse();
   delay (500);
-  int distancia = promedioDistancia ();
+  int distancia = calcularDistancia ();
   bool frenteLibre = false;
   if (distancia > umbral) {
-    while (distancia > umbral && i < tiempoAvance && frenteLibre == false) {  
-      if (i%500 == 0 && i > 0) {   //como tiene un radar en el frente nada más, cada medio segundo gira 90 grados para quedar en la dirección original,
+    while (distancia > umbral && i < tiempoAvance && !frenteLibre) {  
+      if (i%1000 == 0 && i > 0) {   //como tiene un radar en el frente nada más, cada medio segundo gira 90 grados para quedar en la dirección original,
         girarAntiHorario ();   //analiza si el frente está libre, si no lo está gira 90 grados para el otro lado, y sigue avanzando
-        delay(tiempoGiro);              //paralelo al obstáculo hasta que cumpla el tiempoAvance máximo. 
+        delay(tiempoGiroAnti);              //paralelo al obstáculo hasta que cumpla el tiempoAvance máximo. 
         detenerse();      
         delay(500);            
-        distancia = promedioDistancia ();
+        distancia = calcularDistancia ();
         if (distancia > umbral) {
           frenteLibre = true;
         } else {
           girarHorario ();
-          delay (tiempoGiro);
+          delay (tiempoGiroHor);
           detenerse();
           delay(500);
         }
@@ -264,7 +339,7 @@ bool girar90Der () {
         distancia = calcularDistancia ();
       }
     }
-    if (frenteLibre == false) {      //retrocede porque llegó a avanzar hacia el costado
+    if (!frenteLibre) {      //retrocede porque llegó a avanzar hacia el costado
       int tAvanz= i;    //tiempo que avanzó
       i = 0;
       detenerse();
@@ -274,7 +349,7 @@ bool girar90Der () {
       detenerse();
       delay (500);
       girarAntiHorario ();   
-      delay(tiempoGiro);
+      delay(tiempoGiroAnti);
       detenerse();
     } 
   } else {        
@@ -282,50 +357,103 @@ bool girar90Der () {
       detenerse();
       delay (500);      
       girarAntiHorario ();           //deshace el giro (no llegó a avanzar)
-      delay(tiempoGiro);
+      delay(tiempoGiroAnti);
       detenerse();
     }
-  if (frenteLibre == true) {
+  if (frenteLibre) {
     Serial.println("giró bien der 90");
   } else {
     Serial.println("NO giró der 90");
   }
   return frenteLibre;
 }
+*/
+
+bool evadirIzq () {
+  bool frenteLibre;
+  girarAntiHorario ();
+  delay (tiempoGiroAnti);
+  detenerse();
+  delay(100);
+  if (calcularDistancia() < umbral) {
+    frenteLibre = false;
+    girarHorario ();
+    delay (tiempoGiroHor);
+    detenerse ();
+    delay(100);
+  } else {
+    frenteLibre = true;
+  }
+  return frenteLibre;
+}
+
+bool evadirDer () {
+  bool frenteLibre;
+  girarHorario ();
+  delay (tiempoGiroHor);
+  detenerse();
+  delay(100);
+  if (calcularDistancia() < umbral) {
+    frenteLibre = false;
+    girarAntiHorario ();
+    delay (tiempoGiroAnti);
+    detenerse ();
+    delay(100);
+  } else {
+    frenteLibre = true;
+  }
+  return frenteLibre;
+}
 
 void giro180 () {
   detenerse();
-  delay(500);
+  delay(100);
   girarHorario ();
-  delay(2*tiempoGiro);
+  delay(2*tiempoGiroHor);
   detenerse ();
   Serial.println("gira 180");
 }
 
 void navegacionAutomatica() {
-  distancia = calcularDistancia();
-  if (distancia > umbral) {   
+  if (calcularDistancia() > umbral) {
     avanzar();
-    delay(50);
-  } else {
+    delay(10);
+  } 
+  else {
     detenerse();
-    delay(500); //importante!!!! que frene antes de girar 
-    bool evadirDer= girar90Der();
-    if (evadirDer == false) {
-      detenerse();
-      delay(500);
-      bool evadirIzq = girar90Izq();
-      if (evadirIzq == false) {
+    delay (100);
+    marchaAtras ();
+    delay (300);
+    detenerse(); //importante!!!! que frene antes de girar
+    i = random(0,2);
+    if (i == 0) {
+      if (!evadirIzq()) {
         detenerse();
         delay(500);
-        marchaAtras();
-        delay(500);
-        giro180();
+        if (!evadirDer ()) {
+          detenerse();
+          delay(500);
+          marchaAtras();
+          delay(300);
+          giro180();
+        }
       }
+    } else {
+      if (!evadirDer()) {
+        detenerse();
+        delay(500);
+        if (!evadirIzq()) {
+          detenerse();
+          delay(500);
+          marchaAtras();
+          delay(300);
+          giro180();
+        }
+      }
+
     }
   }
 }
-
 
 //conexión a wifi
 
@@ -334,6 +462,52 @@ void manejarCliente(WiFiClient &cliente) {
     delay(1);
   }
   String request = cliente.readStringUntil('\n');
+  if (request.indexOf("/v1") != -1) {
+    Serial.println("velocidad 1");
+    cliente.println("HTTP/1.1 200 OK\nContent-Type: text/html\n\n\n<html><body>velocidad 1</body></html>\n");
+    setearVel (0);
+  }
+  if (request.indexOf("/v2") != -1) {
+    Serial.println("velocidad 2");
+    cliente.println("HTTP/1.1 200 OK\nContent-Type: text/html\n\n\n<html><body>velocidad 2</body></html>\n");
+    setearVel (1);
+  }
+  if (request.indexOf("/v3") != -1) {
+    Serial.println("velocidad 3");
+    cliente.println("HTTP/1.1 200 OK\nContent-Type: text/html\n\n\n<html><body>velocidad 3</body></html>\n");
+    setearVel (2);
+  }
+  if (request.indexOf("/v4") != -1) {
+    Serial.println("velocidad 4");
+    cliente.println("HTTP/1.1 200 OK\nContent-Type: text/html\n\n\n<html><body>velocidad 4</body></html>\n");
+    setearVel (3);
+  }
+  if (request.indexOf("/v5") != -1) {
+    Serial.println("velocidad 5");
+    cliente.println("HTTP/1.1 200 OK\nContent-Type: text/html\n\n\n<html><body>velocidad 5</body></html>\n");
+    setearVel (4);
+  }
+  if (request.indexOf("/giro90hor") != -1) {
+    Serial.println("gira 90 grados en sentido horario");
+    cliente.println("HTTP/1.1 200 OK\nContent-Type: text/html\n\n\n<html><body>gira 90 grados en sentido horario</body></html>\n");
+    girarHorario();
+    delay(tiempoGiroHor);
+    detenerse();
+  }
+
+  if (request.indexOf("/giro90anti") != -1) {
+    Serial.println("gira 90 grados en sentido antihorario");
+    cliente.println("HTTP/1.1 200 OK\nContent-Type: text/html\n\n\n<html><body>gira 90 grados en sentido antihorario</body></html>\n");
+    girarAntiHorario();
+    delay(tiempoGiroAnti);
+    detenerse();
+  }
+
+  if (request.indexOf("/desacelerar") != -1) {
+    Serial.println("desacelera");
+    cliente.println("HTTP/1.1 200 OK\nContent-Type: text/html\n\n\n<html><body>desacelera</body></html>\n");
+    //desacelerar ();
+  }
   if (request.indexOf("/cambiodemodo") != -1) {
     reversa = false;
     manual = !manual;
@@ -341,6 +515,20 @@ void manejarCliente(WiFiClient &cliente) {
     cliente.println("HTTP/1.1 200 OK\nContent-Type: text/html\n\n\n<html><body>cambio de modo</body></html>\n");
     detenerse();
   }
+    if (request.indexOf("/manual") != -1) {
+      reversa = false;
+      manual = !manual;
+      Serial.println("modo manual");
+      cliente.println("HTTP/1.1 200 OK\nContent-Type: text/html\n\n\n<html><body>modo manual</body></html>\n");
+      detenerse();
+    }
+    if (request.indexOf("/automatico") != -1) {
+      reversa = false;
+      manual = !manual;
+      Serial.println("modo automatico");
+      cliente.println("HTTP/1.1 200 OK\nContent-Type: text/html\n\n\n<html><body>modo automático</body></html>\n");
+      detenerse();
+    }
 
   if (manual) {
     if (request.indexOf("/avanzar") != -1) {
@@ -414,29 +602,32 @@ bool clienteNuevo (WiFiClient &cliente) {
 }
   
 void setup() {
+  detenerse ();
+  v = velocidades[2];
   configurarMotor (motor_izq);
   configurarMotor (motor_der);
-  detenerse ();
-  Serial.begin(115200);
   WiFi.softAPConfig(ip_rover, puerta_enlace, mascara);
   WiFi.softAP(nombre_wifi, contra);
   server.begin();
   pinMode(pinLed,OUTPUT);
   pinMode(Trigger, OUTPUT);
   pinMode(Echo, INPUT);
+  pinMode(A0,OUTPUT);
+  detenerse();
   manual = true;
   for (int c = 0;c<4;c++) { //para que no arranque tan rápido el serial y todo eso
     prenderLed ();
-    delay (800);
+    delay (150);
     apagarLed();
-    delay(800);
+    delay (150);
   }
+  Serial.begin(115200);
   Serial.println("\n\n\n===== NUEVA LECTURA =====\n\n\n");
   Serial.println("punto de acceso iniciado");
   Serial.print("ip: ");
   Serial.println(WiFi.softAPIP());
+  detenerse ();
 }
-
 void loop() {
   WiFiClient cliente = server.accept();  //supuestamente se usa accept ahora y no available
   if (cliente) {
@@ -444,10 +635,12 @@ void loop() {
       Serial.print("nuevo cliente, su ip es: ");
       Serial.println(cliente.remoteIP());
     }
-    
+
     manejarCliente (cliente);
-  }    
+  }
+  //VoltBateria();
   if (!manual) {
     navegacionAutomatica ();
-  } 
+  }
+  
 }
